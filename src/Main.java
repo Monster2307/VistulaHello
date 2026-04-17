7ecfcc6e7f17e39c44bb8d78872867738404c369
Pooja Odedara <133855357+Monster2307@users.noreply.github.com>
2026-04-17 22:43:49 +0200

    Final submission for Lab 1 including all tasks with screenshots

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    System.out.println("Hello Vistula");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula");
 
+        int myLuckNumber = 7;
+        String message = "Welcome to Java!";
+
+        System.out.println(message + " Your lucky number is: " + myLuckNumber);
+    }
 }
